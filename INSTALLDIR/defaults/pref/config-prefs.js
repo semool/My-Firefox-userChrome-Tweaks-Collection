0c53bdd0e43297fbc25d3e41d8601ce742516e09
semool <semool@protonmail.com>
2018-05-09 19:21:24 +0200

    Workaround for FF60 Bug

--- INSTALLDIR/defaults/pref/config-prefs.js
+++ INSTALLDIR/defaults/pref/config-prefs.js
@@ -1,2 +1,5 @@
-﻿pref("general.config.obscure_value", 0);
+﻿/* Dont work with FF60
+ * Put this prefs also in channel-prefs.js as workaround */
+
+pref("general.config.obscure_value", 0);
 pref("general.config.filename", "config.js");
