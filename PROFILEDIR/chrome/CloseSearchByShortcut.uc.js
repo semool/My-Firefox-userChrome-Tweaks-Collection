33466be9a4d9fee321913b46b019f933cd602e7e
semool <semool@protonmail.com>
2018-07-23 13:18:29 +0200

    Remove CloseSearchByShortcut.uc.js

--- PROFILEDIR/chrome/CloseSearchByShortcut.uc.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  if (location == 'chrome://browser/content/browser.xul') {
-    document.getElementById('cmd_find').setAttribute('oncommand',
-      'if (gFindBar.hidden) { gFindBar.onFindCommand() } else { gFindBar.close() }');
-  };
-})();
