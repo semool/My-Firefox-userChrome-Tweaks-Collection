17e367e52ffdd5933731dae882b01c29960b9054
semool <semool@protonmail.com>
2018-09-16 12:15:27 +0200

    - readd newtab button - replace gBrowser.addTab (dont work in >=FF63) with BrowserOpenTab

--- PROFILEDIR/chrome/DblClickNewTab.uc.js
+++ PROFILEDIR/chrome/DblClickNewTab.uc.js
@@ -1,13 +1,19 @@
+/* Neuen Tab per Doppelklick öffnen *******************************************/
 (function() {
-
-/* Neuen Tab per Doppelklick öffnen *******************************************/ 
 gBrowser.tabContainer.addEventListener('dblclick', function(event) {
 	if (event.target.localName != 'tabs' && event.target.localName != 'toolbarbutton') {
 		return;
 	}
-	gBrowser.selectedTab = gBrowser.addTab('about:home');
+	gBrowser.selectedTab = BrowserOpenTab();
 	event.preventDefault();
 	event.stopPropagation();
 });
-
 })();
+
+/* Alternative Methode ******************************************************
+location == "chrome://browser/content/browser.xul" && (function () {
+	(document.getElementById("tabbrowser-tabs") || gBrowser.mTabBox).addEventListener('dblclick', function (e) {
+		e.preventDefault();
+			e.button == 0 && e.originalTarget.localName == "box" && BrowserOpenTab()
+	}, false)
+})()*/
